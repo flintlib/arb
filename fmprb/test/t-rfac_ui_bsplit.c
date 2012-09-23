61e04b8bf91614176ebd769c2f890371e4d1d5ef
Fredrik Johansson <fredrik.johansson@gmail.com>
2012-09-24 00:13:37 +0200

    fix some copyright headers

--- fmprb/test/t-rfac_ui_bsplit.c
+++ fmprb/test/t-rfac_ui_bsplit.c
@@ -1,25 +1,25 @@
 /*=============================================================================
 
-This file is part of fmprb.
+    This file is part of fmprb.
 
-fmprb is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+    fmprb is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-fmprb is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+    fmprb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with fmprb; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+    You should have received a copy of the GNU General Public License
+    along with fmprb; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 =============================================================================*/
 /******************************************************************************
 
-Copyright (C) 2012 Fredrik Johansson
+    Copyright (C) 2012 Fredrik Johansson
 
 ******************************************************************************/
 
