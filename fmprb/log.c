dd965a1662ce2d39a69d1636c3b67c80527efdb4
Fredrik Johansson <fredrik.johansson@gmail.com>
2013-03-04 15:39:32 +0100

    improve fmprb_log (slight speedup, detect powers of two, support large exponents)

--- fmprb/log.c
+++ fmprb/log.c
@@ -25,22 +25,137 @@
 
 #include "fmprb.h"
 
+#define BIG_EXPONENT_BITS 20
+#define BIG_EXPONENT (1L << BIG_EXPONENT_BITS)
+
 void
-fmprb_log(fmprb_t z, const fmprb_t x, long prec)
+fmpr_log1p_ubound(fmpr_t y, const fmpr_t x)
+{
+    if (fmpr_is_special(x))
+    {
+        if (fmpr_is_zero(x))
+            fmpr_zero(y);
+        else if (fmpr_is_pos_inf(x))
+            fmpr_pos_inf(y);
+        else
+            fmpr_nan(y);
+    }
+    else if (fmpr_sgn(x) < 0)
+    {
+        fmpr_nan(y);
+    }
+    else
+    {
+        fmpz_t exp;
+        fmpz_init(exp);
+        fmpz_add_ui(exp, fmpr_expref(x), fmpz_bits(fmpr_manref(x)));
+
+        if (fmpz_cmp_si(exp, -8) < 0)
+        {
+            fmpr_set(y, x);
+        }
+        else if (fmpz_cmp_si(exp, BIG_EXPONENT) > 0)
+        {
+            /* bound 1+x by 2^n */
+            fmpr_add_ui(y, x, 1, FMPRB_RAD_PREC, FMPR_RND_UP);
+            fmpz_add_ui(fmpr_manref(y), fmpr_expref(y), fmpz_bits(fmpr_manref(y)));
+
+            /* log(2) < 11629080 / 2^24 */
+            fmpz_mul_ui(fmpr_manref(y), fmpr_manref(y), 11629080);
+            fmpz_set_si(fmpr_expref(y), -24);
+            _fmpr_normalise(fmpr_manref(y), fmpr_expref(y), FMPRB_RAD_PREC, FMPR_RND_UP);
+        }
+        else
+        {
+            fmpr_log1p(y, x, FMPRB_RAD_PREC, FMPR_RND_UP);
+        }
+
+        fmpz_clear(exp);
+    }
+}
+
+void
+fmprb_log_fmpr(fmprb_t y, const fmpr_t x, long prec)
 {
     long r;
+    fmpz_t exp;
 
-    if (fmprb_contains_zero(x) || fmpr_sgn(fmprb_midref(x)) < 0)
+    if (fmpr_is_special(x))
     {
-        fmpr_nan(fmprb_midref(z));
-        fmpr_pos_inf(fmprb_radref(z));
+        if (fmpr_is_pos_inf(x))
+        {
+            fmpr_pos_inf(fmprb_midref(y));
+            fmpr_zero(fmprb_radref(y));
+        }
+        else
+        {
+            fmpr_nan(fmprb_midref(y));
+            fmpr_pos_inf(fmprb_radref(y));
+        }
         return;
     }
 
+    if (fmpz_sgn(fmpr_manref(x)) < 0)
+    {
+        fmpr_nan(fmprb_midref(y));
+        fmpr_pos_inf(fmprb_radref(y));
+        return;
+    }
+
+    if (fmpz_is_one(fmpr_manref(x)))
+    {
+        fmpz_init(exp);
+        fmpz_set(exp, fmpr_expref(x));
+        fmprb_const_log2(y, prec + 2);
+        fmprb_mul_fmpz(y, y, exp, prec);
+        fmpz_clear(exp);
+        return;
+    }
+
+    fmpz_init(exp);
+    fmpz_add_ui(exp, fmpr_expref(x), fmpz_bits(fmpr_manref(x)));
+
+    if (*exp >= -BIG_EXPONENT && *exp <= BIG_EXPONENT)
+    {
+        r = fmpr_log(fmprb_midref(y), x, prec, FMPR_RND_DOWN);
+        fmpr_set_error_result(fmprb_radref(y), fmprb_midref(y), r);
+    }
+    else
+    {
+        fmpr_t t;
+        fmprb_t c;
+        long wp;
+
+        fmpr_init(t);
+        fmprb_init(c);
+
+        fmpz_neg(exp, exp);
+        fmpr_mul_2exp_fmpz(t, x, exp);
+
+        wp = prec + 4 - fmpz_bits(exp);
+        wp = FLINT_MAX(wp, 4);
+
+        r = fmpr_log(fmprb_midref(y), t, wp, FMPR_RND_DOWN);
+        fmpr_set_error_result(fmprb_radref(y), fmprb_midref(y), r);
+
+        fmprb_const_log2(c, prec + 4);
+        fmprb_mul_fmpz(c, c, exp, prec + 4);
+
+        fmprb_sub(y, y, c, prec);
+
+        fmpr_clear(t);
+        fmprb_clear(c);
+    }
+
+    fmpz_clear(exp);
+}
+
+void
+fmprb_log(fmprb_t y, const fmprb_t x, long prec)
+{
     if (fmprb_is_exact(x))
     {
-        r = fmpr_log(fmprb_midref(z), fmprb_midref(x), prec, FMPR_RND_DOWN);
-        fmpr_set_error_result(fmprb_radref(z), fmprb_midref(z), r);
+        fmprb_log_fmpr(y, fmprb_midref(x), prec);
     }
     else
     {
@@ -54,34 +169,41 @@ fmprb_log(fmprb_t z, const fmprb_t x, long prec)
         fmpr_t err;
         fmpr_init(err);
         fmpr_sub(err, fmprb_midref(x), fmprb_radref(x), FMPRB_RAD_PREC, FMPR_RND_DOWN);
-        fmpr_div(err, fmprb_radref(x), err, FMPRB_RAD_PREC, FMPR_RND_UP);
-        fmpr_log1p(err, err, FMPRB_RAD_PREC, FMPR_RND_UP);
 
-        r = fmpr_log(fmprb_midref(z), fmprb_midref(x), prec, FMPR_RND_DOWN);
-        fmpr_add_error_result(fmprb_radref(z), err, fmprb_midref(z), r,
-            FMPRB_RAD_PREC, FMPR_RND_UP);
+        if (fmpr_sgn(err) <= 0)
+        {
+            fmpr_pos_inf(err);
+        }
+        else
+        {
+            fmpr_div(err, fmprb_radref(x), err, FMPRB_RAD_PREC, FMPR_RND_UP);
+            fmpr_log1p_ubound(err, err);
+        }
+
+        fmprb_log_fmpr(y, fmprb_midref(x), prec);
+        fmpr_add(fmprb_radref(y), fmprb_radref(y), err, FMPRB_RAD_PREC, FMPR_RND_UP);
         fmpr_clear(err);
     }
 
-    fmprb_adjust(z);
+    fmprb_adjust(y);
 }
 
 void
 fmprb_log_ui(fmprb_t z, ulong x, long prec)
 {
-    fmprb_t t;
-    fmprb_init(t);
-    fmprb_set_ui(t, x);
-    fmprb_log(z, t, prec);
-    fmprb_clear(t);
+    fmpr_t t;
+    fmpr_init(t);
+    fmpr_set_ui(t, x);
+    fmprb_log_fmpr(z, t, prec);
+    fmpr_clear(t);
 }
 
 void
 fmprb_log_fmpz(fmprb_t z, const fmpz_t x, long prec)
 {
-    fmprb_t t;
-    fmprb_init(t);
-    fmprb_set_fmpz(t, x);
-    fmprb_log(z, t, prec);
-    fmprb_clear(t);
+    fmpr_t t;
+    fmpr_init(t);
+    fmpr_set_fmpz(t, x);
+    fmprb_log_fmpr(z, t, prec);
+    fmpr_clear(t);
 }
--- fmprb/test/t-log.c
+++ fmprb/test/t-log.c
@@ -35,6 +35,7 @@ int main()
 
     flint_randinit(state);
 
+    /* compare with mpfr */
     for (iter = 0; iter < 100000; iter++)
     {
         fmprb_t a, b;
@@ -49,7 +50,7 @@ int main()
 
         do {
             fmprb_randtest(a, state, 1 + n_randint(state, 200), 10);
-        } while (fmprb_contains_zero(a) || fmpr_sgn(fmprb_midref(a)) < 0);
+        } while (fmprb_contains_nonpositive(a));
 
         fmprb_randtest(b, state, 1 + n_randint(state, 200), 10);
         fmprb_get_rand_fmpq(q, state, a, 1 + n_randint(state, 200));
@@ -81,8 +82,64 @@ int main()
         mpfr_clear(t);
     }
 
+    /* test large numbers */
+    for (iter = 0; iter < 10000; iter++)
+    {
+        fmprb_t a, b, ab, lab, la, lb, lalb;
+        long prec = 2 + n_randint(state, 400);
+
+        fmprb_init(a);
+        fmprb_init(ab);
+        fmprb_init(lab);
+        fmprb_init(la);
+        fmprb_init(lb);
+        fmprb_init(lalb);
+
+        do {
+            fmprb_randtest(a, state, 1 + n_randint(state, 400), 400);
+        } while (fmprb_contains_nonpositive(a));
+
+        do {
+            fmprb_randtest(b, state, 1 + n_randint(state, 400), 400);
+        } while (fmprb_contains_nonpositive(a));
+
+        fmprb_log(la, a, prec);
+        fmprb_log(lb, b, prec);
+        fmprb_mul(ab, a, b, prec);
+        fmprb_log(lab, ab, prec);
+        fmprb_add(lalb, la, lb, prec);
+
+        if (!fmprb_overlaps(lab, lalb))
+        {
+            printf("FAIL: containment\n\n");
+            printf("a = "); fmprb_print(a); printf("\n\n");
+            printf("b = "); fmprb_print(b); printf("\n\n");
+            printf("la = "); fmprb_print(la); printf("\n\n");
+            printf("lb = "); fmprb_print(lb); printf("\n\n");
+            printf("ab = "); fmprb_print(ab); printf("\n\n");
+            printf("lab = "); fmprb_print(lab); printf("\n\n");
+            printf("lalb = "); fmprb_print(lalb); printf("\n\n");
+            abort();
+        }
+
+        fmprb_log(a, a, prec);
+        if (!fmprb_overlaps(a, la))
+        {
+            printf("FAIL: aliasing\n\n");
+            abort();
+        }
+
+        fmprb_clear(a);
+        fmprb_clear(ab);
+        fmprb_clear(lab);
+        fmprb_clear(la);
+        fmprb_clear(lb);
+        fmprb_clear(lalb);
+    }
+
     flint_randclear(state);
     _fmpz_cleanup();
     printf("PASS\n");
     return EXIT_SUCCESS;
 }
+
