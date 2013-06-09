cb15277968649f9f59ad020a98336907ebc09729
Fredrik Johansson <fredrik.johansson@gmail.com>
2013-06-09 16:34:53 +0200

    slightly speed up pi computation

--- fmprb/const_pi_chudnovsky.c
+++ fmprb/const_pi_chudnovsky.c
@@ -41,12 +41,14 @@ fmprb_const_pi_chudnovsky(fmprb_t s, long prec)
     fmpz_poly_set_str(series->P, "4  5 -46 108 -72");
     fmpz_poly_set_str(series->Q, "4  0 0 0 10939058860032000");
 
-    prec += FLINT_CLOG2(prec);
+    prec += FLINT_CLOG2(prec) + 5;
     fmprb_hypgeom_infsum(s, t, series, prec, prec);
-    fmprb_sqrt_ui(u, 640320, prec);
-    fmprb_mul_ui(u, u, 640320 / 12, prec);
-    fmprb_mul(u, u, t, prec);
-    fmprb_div(s, u, s, prec);
+
+    fmprb_rsqrt_ui(u, 640320, prec);
+    fmprb_mul(s, s, u, prec);
+
+    fmprb_mul_ui(t, t, 640320 / 12, prec);
+    fmprb_div(s, t, s, prec);
 
     hypgeom_clear(series);
     fmprb_clear(t);
