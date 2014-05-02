dd959c3ef9b7247d2fb162cb4de5b571eca3756d
Fredrik Johansson <fredrik.johansson@gmail.com>
2014-05-03 01:38:53 +0200

    more subtraction methods

--- arf.h
+++ arf.h
@@ -916,6 +916,9 @@ int arf_add_ui(arf_ptr z, arf_srcptr x, ulong y, long prec, arf_rnd_t rnd);
 int arf_add_fmpz(arf_ptr z, arf_srcptr x, const fmpz_t y, long prec, arf_rnd_t rnd);
 
 int arf_sub(arf_ptr z, arf_srcptr x, arf_srcptr y, long prec, arf_rnd_t rnd);
+int arf_sub_si(arf_ptr z, arf_srcptr x, long y, long prec, arf_rnd_t rnd);
+int arf_sub_ui(arf_ptr z, arf_srcptr x, ulong y, long prec, arf_rnd_t rnd);
+int arf_sub_fmpz(arf_ptr z, arf_srcptr x, const fmpz_t y, long prec, arf_rnd_t rnd);
 
 #ifdef __cplusplus
 }
