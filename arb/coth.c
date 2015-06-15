ec2a90f565df7bab1e3aee7b04764d6a3f9222b3
Fredrik Johansson <fredrik.johansson@gmail.com>
2015-06-15 20:20:21 +0200

    improve arb_coth and arb_tanh for large input

--- arb/coth.c
+++ arb/coth.c
@@ -29,28 +29,36 @@ void
 arb_coth(arb_t y, const arb_t x, long prec)
 {
     arb_t t, u;
+    int sign = arf_sgn(arb_midref(x)) < 0;
 
     arb_init(t);
     arb_init(u);
 
     arb_mul_2exp_si(t, x, 1);
 
-    if (arf_sgn(arb_midref(x)) >= 0)
-    {
+    if (!sign)
         arb_neg(t, t);
-        arb_expm1(t, t, prec + 4);
-        arb_add_ui(y, t, 2, prec + 4);
-        arb_div(y, y, t, prec);
-        arb_neg(y, y);
+
+    if (arf_cmpabs_2exp_si(arb_midref(x), 1) > 0)
+    {
+        /* coth(x) = 1 + 2 exp(-2x) / (1 - exp(-2x)) */
+        arb_exp(t, t, prec + 4);
+        arb_sub_ui(u, t, 1, prec + 4);
+        arb_div(y, t, u, prec + 4);
+        arb_mul_2exp_si(y, y, 1);
+        arb_sub_ui(y, y, 1, prec);
     }
     else
     {
+        /* coth(x) = (exp(2x) + 1) / (exp(2x) - 1) */
         arb_expm1(t, t, prec + 4);
         arb_add_ui(y, t, 2, prec + 4);
         arb_div(y, y, t, prec);
     }
+
+    if (!sign)
+        arb_neg(y, y);
 
     arb_clear(t);
     arb_clear(u);
 }
-
