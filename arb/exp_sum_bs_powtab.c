7e7f47d734ed88bdd0f4f1959fdf34445028d20b
Alexander Kobel <akobel@mpi-inf.mpg.de>
2015-07-21 13:51:36 +0200

    remove ab variable in _arb_compute_bs_exponents
    to avoid compiler warnings; self-assignment ab = ab used to give
    a warning in clang

--- arb/exp_sum_bs_powtab.c
+++ arb/exp_sum_bs_powtab.c
@@ -33,7 +33,7 @@
 long
 _arb_compute_bs_exponents(long * tab, long n)
 {
-    long a, b, aa, ab, ba, bb, length;
+    long a, b, aa, /* ab, */ ba, bb, length;
 
     if (n == 1)
     {
@@ -66,7 +66,7 @@ _arb_compute_bs_exponents(long * tab, long n)
     {
         /* split a -> aa, ab  and b -> ba, bb */
         aa = a >> 1;
-        ab = a - aa;
+        /* ab = a - aa; */
         ba = b >> 1;
         bb = b - ba;
 
@@ -98,9 +98,6 @@ _arb_compute_bs_exponents(long * tab, long n)
         a = aa;
         b = bb;
     }
-
-    /* Prevent compiler warning (ab is used to make the algorithm clear). */
-    ab = ab;
 
     /* we always include x^1 in the table, even if the binary splitting
        terminates at step length 2 */
