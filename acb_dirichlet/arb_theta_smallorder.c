49e03f8a1ffba86143393340a1af454c2ba7c1fd
Pascal <molin.maths@gmail.com>
2016-06-15 17:48:38 +0200

    split theta file

--- acb_dirichlet/arb_theta_smallorder.c
+++ acb_dirichlet/arb_theta_smallorder.c
@@ -26,18 +26,6 @@
 #include "acb_dirichlet.h"
 #include "acb_poly.h"
 
-/* x = Pi / q * t^2 */
-static void
-acb_dirichlet_arb_theta_argt(arb_t x, ulong q, const arb_t t, slong prec)
-{
-    arb_const_pi(x, prec);
-    arb_div_ui(x, x, q, prec);
-    arb_mul(x, x, t, prec);
-    arb_mul(x, x, t, prec);
-    arb_neg(x, x);
-    arb_exp(x, x, prec);
-}
-
 /* small order, multiply by chi at the end */
 void
 acb_dirichlet_arb_theta_smallorder(acb_t res, const arb_t x, int parity, const ulong *a, const acb_dirichlet_powers_t z, slong len, slong prec)
@@ -86,64 +74,4 @@ acb_dirichlet_arb_theta_smallorder(acb_t res, const arb_t x, int parity, const u
     arb_clear(xk2);
     arb_clear(x2);
     arb_clear(dx);
-}
-
-void
-acb_dirichlet_arb_theta_naive(acb_t res, const arb_t x, int parity, const ulong *a, const acb_dirichlet_powers_t z, slong len, slong prec)
-{
-    slong k;
-    arb_t xk2, dx, x2;
-    acb_t zk;
-    arb_init(xk2);
-    arb_init(dx);
-    arb_init(x2);
-    acb_init(zk);
-
-    arb_set(dx, x);
-    arb_set(xk2, dx);
-    arb_mul(x2, dx, dx, prec);
-
-    acb_set_arb(res, xk2);
-
-    /* TODO: reduce prec */
-    for (k = 2; k < len; k++)
-    {
-        arb_mul(dx, dx, x2, prec);
-        arb_mul(xk2, xk2, dx, prec);
-        if (a[k] != ACB_DIRICHLET_CHI_NULL)
-        {
-            acb_dirichlet_power(zk, z, a[k], prec); 
-            if (parity)
-                acb_mul_si(zk, zk, k, prec);
-            acb_addmul_arb(res, zk, xk2, prec);
-        }
-    }
-
-    arb_clear(xk2);
-    arb_clear(x2);
-    arb_clear(dx);
-    acb_clear(zk);
-}
-
-void
-acb_dirichlet_chi_theta(acb_t res, const acb_dirichlet_group_t G, const acb_dirichlet_char_t chi, const arb_t t, slong prec)
-{
-    slong len;
-    ulong * a;
-    arb_t x;
-    acb_dirichlet_powers_t z;
-
-    len = acb_dirichlet_theta_length(G->q, t, prec);
-
-    a = flint_malloc(len * sizeof(ulong));
-    acb_dirichlet_ui_chi_vec(a, G, chi, len);
-    acb_dirichlet_powers_init(z, chi->order, len, prec);
-
-    arb_init(x);
-    acb_dirichlet_arb_theta_argt(x, G->q, t, prec);
-    acb_dirichlet_arb_theta_naive(res, x, chi->parity, a, z, len, prec);
-
-    arb_clear(x);
-    flint_free(a);
-    acb_dirichlet_powers_clear(z);
 }
