90404742461bf4c0ac3dda5c8286ee654d5d3252
Pascal <molin.maths@gmail.com>
2016-09-21 15:04:01 +0200

    vocabulary: number/log/index

---
+++ acb_dirichlet/index_conrey.c
@@ -0,0 +1,38 @@
+/*=============================================================================
+
+    This file is part of ARB.
+
+    ARB is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    ARB is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ARB; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+=============================================================================*/
+/******************************************************************************
+
+    Copyright (C) 2016 Pascal Molin
+
+******************************************************************************/
+
+#include "acb_dirichlet.h"
+
+ulong
+acb_dirichlet_index_conrey(const acb_dirichlet_group_t G, const acb_dirichlet_conrey_t x)
+{
+    slong k;
+    ulong j = 0;
+
+    for (k = 0; k < G->num; k++)
+        j = j * G->P[k].phi + x->log[k];
+
+    return j;
+}
