e3621790af523db3706b317eba1c67ddff99f96f
Pascal <molin.maths@gmail.com>
2016-10-06 08:52:22 +0200

    bluestein dft, WIP

--- acb_dirichlet/dft_convol_fft.c
+++ acb_dirichlet/dft_convol_fft.c
@@ -16,14 +16,25 @@ void
 acb_dirichlet_dft_convol_pad(acb_ptr fp, acb_ptr gp, acb_srcptr f, acb_srcptr g, slong n, slong np)
 {
     slong k;
+
+    if (np < 2 * n - 1)
+    {
+        flint_printf("dft_convol_pad: overlapping padding %ld < 2*%ld-1\n", np, n);
+        abort();
+    }
+
     for (k = 0; k < n; k++)
         acb_set(gp + k, g + k);
+    for (; k < np; k++)
+        acb_zero(gp + k);
+
     for (k = 0; k < n; k++)
         acb_set(fp + k, f + k);
     for (k = 1; k < n; k++)
         acb_set(fp + np - k, f + n - k);
     for (k = n; k <= np - n; k++)
         acb_zero(fp + k);
+
 }
 
 void
@@ -37,22 +48,58 @@ acb_dirichlet_dft_inverse_cyc(acb_ptr w, acb_srcptr v, slong len, slong prec)
 }
 
 void
-acb_dirichlet_dft_convol_fft(acb_ptr w, acb_srcptr f, acb_srcptr g, slong len, slong prec)
+acb_dirichlet_dft_convol_rad2_precomp(acb_ptr w, acb_srcptr f, acb_srcptr g, slong len, const acb_dirichlet_dft_rad2_t rad2, slong prec)
 {
-    int e;
-    slong k, np;
+    slong np;
     acb_ptr fp, gp;
-    acb_dirichlet_dft_rad2_t dft;
-    e = n_clog(2 * len + 1, 2);
-    acb_dirichlet_dft_rad2_init(dft, e, prec);
-    np = dft->n;
+    np = rad2->n;
+
+    flint_printf("\nf\n");
+    acb_vec_printd_index(f, len, 10);
+    flint_printf("\ng\n");
+    acb_vec_printd_index(g, len, 10);
+
     fp = _acb_vec_init(np);
     gp = _acb_vec_init(np);
     acb_dirichlet_dft_convol_pad(fp, gp, f, g, len, np);
-    acb_dirichlet_dft_rad2_precomp(fp, dft, prec);
-    acb_dirichlet_dft_rad2_precomp(gp, dft, prec);
+
+    flint_printf("\nF\n");
+    acb_vec_printd_index(fp, np, 10);
+    flint_printf("\nG\n");
+    acb_vec_printd_index(gp, np, 10);
+
+    acb_dirichlet_dft_rad2_precomp(fp, rad2, prec);
+
+    flint_printf("\nDFT F\n");
+    acb_vec_printd_index(fp, np, 10);
+
+    acb_dirichlet_dft_rad2_precomp(gp, rad2, prec);
+
+    flint_printf("\nDFT G\n");
+    acb_vec_printd_index(gp, np, 10);
+
     _acb_vec_kronecker_mul(gp, gp, fp, np, prec);
-    acb_dirichlet_dft_inverse_rad2_precomp(gp, dft, prec);
-    for (k = 0; k < len; k++)
-        acb_set(w + k, gp + k);
+
+    flint_printf("\n(DFT F)(DFT G)=DFT(F*G)\n");
+    acb_vec_printd_index(gp, np, 10);
+
+    acb_dirichlet_dft_inverse_rad2_precomp(gp, rad2, prec);
+
+    flint_printf("\nF*G\n");
+    acb_vec_printd_index(gp, np, 10);
+
+    _acb_vec_set(w, gp, len);
+    _acb_vec_clear(fp, np);
+    _acb_vec_clear(gp, np);
+}
+
+void
+acb_dirichlet_dft_convol_rad2(acb_ptr w, acb_srcptr f, acb_srcptr g, slong len, slong prec)
+{
+    int e;
+    acb_dirichlet_dft_rad2_t dft;
+    e = n_clog(2 * len - 1, 2);
+    acb_dirichlet_dft_rad2_init(dft, e, prec);
+    acb_dirichlet_dft_convol_rad2_precomp(w, f, g, len, dft, prec);
+    acb_dirichlet_dft_rad2_clear(dft);
 }
---
+++ acb_dirichlet/test/t-convol.c
@@ -0,0 +1,118 @@
+/*
+    Copyright (C) 2016 Pascal Molin
+
+    This file is part of Arb.
+
+    Arb is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License (LGPL) as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.  See <http://www.gnu.org/licenses/>.
+*/
+
+#include "acb_dirichlet.h"
+
+typedef void (*do_f) (acb_ptr z, acb_srcptr x, acb_srcptr y, slong len, slong prec);
+
+void
+check_vec_eq_prec(acb_srcptr w1, acb_srcptr w2, slong len, slong prec, slong digits, ulong q, char f1[], char f2[])
+{
+    slong i;
+
+    for (i = 0; i < len; i++)
+    {
+        if (!acb_overlaps(w1 + i, w2 + i))
+        {
+            flint_printf("FAIL\n\n");
+            flint_printf("q = %wu, size = %wu\n", q, len);
+            flint_printf("\nDFT differ from index %ld / %ld \n", i, len);
+            flint_printf("\n%s =\n", f1);
+            acb_vec_printd_index(w1, len, digits);
+            flint_printf("\n%s =\n", f2);
+            acb_vec_printd_index(w2, len, digits);
+            flint_printf("\n\n");
+            abort();
+        }
+        else if (!acb_is_zero(w1+i) && (acb_rel_accuracy_bits(w1 + i) < 30
+                || acb_rel_accuracy_bits(w2 + i) < 30))
+        {
+            flint_printf("FAIL\n\n");
+            flint_printf("q = %wu\n", q);
+            flint_printf("\nDFT inaccurate from index %ld / %ld \n", i, len);
+            flint_printf("\nnaive =\n");
+            acb_printd(w1 + i, digits);
+            flint_printf("\nfast =\n");
+            acb_printd(w2 + i, digits);
+            flint_printf("\nerrors %ld & %ld [prec = %wu]\n",
+                    acb_rel_accuracy_bits(w1 + i),
+                    acb_rel_accuracy_bits(w2 + i), prec);
+            abort();
+        }
+    }
+}
+
+
+int main()
+{
+
+    slong k;
+    slong prec = 100, digits = 30;
+    slong nq = 13;
+    ulong q[13] = { 2, 3, 4, 5, 6, 23, 10, 15, 30, 59, 308, 335, 961};
+    flint_rand_t state;
+
+    slong f, nf = 2;
+    do_f func[2] = { acb_dirichlet_dft_convol_naive, acb_dirichlet_dft_convol_rad2 };
+    char * name[4] = { "naive", "rad2" };
+
+    flint_printf("convol....");
+    fflush(stdout);
+
+    flint_randinit(state);
+
+    for (k = 0; k < nq; k++)
+    {
+        slong i;
+        acb_ptr z1, z2, x, y;
+
+        z1 = _acb_vec_init(q[k]);
+        z2 = _acb_vec_init(q[k]);
+        x = _acb_vec_init(q[k]);
+        y = _acb_vec_init(q[k]);
+
+        for (i = 0; i < q[k]; i++)
+        {
+            acb_set_si(x + i, q[k] - i);
+            acb_set_si(y + i, i * i);
+            /*
+            acb_set_si(x + i, n_randint(state, q[k]));
+            acb_set_si(y + i, n_randint(state, q[k]));
+            */
+        }
+
+        for (f = 0; f < nf; f++)
+        {
+
+            acb_ptr z = (f == 0) ? z1 : z2;
+
+            func[f](z, x, y, q[k], prec);
+
+            if (f == 0)
+                continue;
+
+            check_vec_eq_prec(z1, z2, q[k], prec, digits, q[k], name[0], name[f]);
+
+        }
+
+        _acb_vec_clear(x, q[k]);
+        _acb_vec_clear(y, q[k]);
+        _acb_vec_clear(z1, q[k]);
+        _acb_vec_clear(z2, q[k]);
+    }
+
+    flint_randclear(state);
+    flint_cleanup();
+    flint_printf("PASS\n");
+    return EXIT_SUCCESS;
+}
+
+
