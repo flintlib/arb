f8bd2155e93ad593735f823753e56a98d2b8c6eb
Fredrik Johansson <fredrik.johansson@gmail.com>
2016-11-24 11:44:42 +0100

    deflated Hurwitz zeta function: use explicit formula at s = 1

--- acb_poly/zeta_series.c
+++ acb_poly/zeta_series.c
@@ -29,6 +29,15 @@ _acb_poly_zeta_cpx_series(acb_ptr z, const acb_t s, const acb_t a, int deflate, 
         return;
     }
 
+    if (acb_is_one(s) && deflate && d == 1)
+    {
+        acb_digamma(z, a, prec);
+        acb_neg(z, z);
+        if (!acb_is_finite(z))  /* todo: in digamma */
+            acb_indeterminate(z);
+        return;
+    }
+
     is_real = const_is_real = 0;
 
     if (acb_is_real(s) && acb_is_real(a))
