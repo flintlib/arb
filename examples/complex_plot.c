f0431de93ea4ace1d3295a8e6ea22fdf721ff26f
fredrik <fredrik.johansson@gmail.com>
2018-04-06 22:37:37 +0200

    some more color functions

--- examples/complex_plot.c
+++ examples/complex_plot.c
@@ -6,6 +6,11 @@
 #include "acb_modular.h"
 #include "flint/profiler.h"
 
+/* some useful color operations */
+#define CLAMP(y) FLINT_MAX(0.0, FLINT_MIN((y), 1.0))
+#define BLEND(x,y) (0.5*(x) + 0.5*(y))
+#define DODGE(a,b) ((a) / ((1.0 - (b)) + 1/256.0))
+
 /* HLS algorithm from python's colorsys module */
 static double
 vv(double m1, double m2, double hue)
@@ -44,6 +49,84 @@ hls_to_rgb(double *R, double *G, double *B, double h, double l, double s)
     *B = vv(m1, m2, h - 1/3.);
 }
 
+static void
+rgb_to_hls(double *H, double *L, double *S, double R, double G, double B)
+{
+    double h, l, s, hi, lo, d;
+
+    hi = FLINT_MAX(FLINT_MAX(R, G), B);
+    lo = FLINT_MIN(FLINT_MIN(R, G), B);
+
+    l = 0.5 * (lo + hi);
+    d = hi - lo;
+
+    if (hi == lo)
+    {
+        s = 0.0;
+        h = 0.0;
+    }
+    else
+    {
+        if (l <= 0.5)
+            s = d / (hi + lo);
+        else
+            s = d / (2.0 - hi - lo);
+
+        if (d == 0.0)
+            d = 1.0;
+
+        if (R == hi)
+            h = (G - B) / d;
+        else if (G == hi)
+            h = (B - R) / d + 2.0;
+        else
+            h = (R - G) / d + 4.0;
+
+        h = h / 6.0;
+        if (h < 0.0)
+            h += 1.0;
+    }
+
+    *H = h;
+    *L = l;
+    *S = s;
+}
+
+/* color balance algorithm from gimp */
+static double balance_channel(double value, double l,
+    double shadows, double midtones, double highlights)
+{
+    double a = 0.25, b = 0.333, scale = 0.7;
+
+    shadows    *= CLAMP((l - b) / (-a) + 0.5) * scale;
+    midtones   *= CLAMP((l - b) / ( a) + 0.5) *
+                  CLAMP((l + b - 1.0) / (-a) + 0.5) * scale;
+    highlights *= CLAMP((l + b - 1.0) / ( a) + 0.5) * scale;
+
+    value += shadows;
+    value += midtones;
+    value += highlights;
+    return CLAMP(value);
+}
+
+static void balance(double * R, double * G, double * B,
+    double ra, double rb, double rc,  /* red shadows, midtones, highlights */
+    double ga, double gb, double gc,  /* green */
+    double ba, double bb, double bc)  /* blue */
+{
+    double h, l, s;
+    double h2, l2, s2;
+
+    rgb_to_hls(&h, &l, &s, *R, *G, *B);
+
+    *R = balance_channel(*R, *R, ra, rb, rc);
+    *G = balance_channel(*G, *G, ga, gb, gc);
+    *B = balance_channel(*B, *B, ba, bb, bc);
+
+    /* preserve lightness */
+    rgb_to_hls(&h2, &l2, &s2, *R, *G, *B);
+    hls_to_rgb(R, G, B, h2, l, s2);
+}
 
 #define PI 3.1415926535898
 
@@ -70,6 +153,32 @@ color_function(double * R, double * G, double * B, const acb_t z, int mode)
     if (!acb_is_finite(z) || acb_rel_accuracy_bits(z) < 4)
     {
         *R = *G = *B = 0.5;
+        return;
+    }
+
+    if (mode >= 2)
+    {
+        double R1, G1, B1;
+        double R2, G2, B2;
+
+        /* combine both color functions */
+        color_function(&R1, &G1, &B1, z, 0);
+        color_function(&R2, &G2, &B2, z, 1);
+
+        *R = BLEND(R1, CLAMP(DODGE(R1, R2)));
+        *G = BLEND(G1, CLAMP(DODGE(G1, G2)));
+        *B = BLEND(B1, CLAMP(DODGE(B1, B2)));
+
+        /* then play with the levels */
+        if (mode == 3)
+            balance(R, G, B, 0.0, -0.5, 0.2, 0.0, 0.0, -0.1, 0.0, -1.0, -0.2);
+        else if (mode == 4)
+            balance(R, G, B, 0.0, -0.5, 0.2, 0.0, 0.5, -0.1, 0.0, -0.3, -1.0);
+        else if (mode == 5)
+            balance(R, G, B, 0.0, -0.5, -1.0, 0.0, -0.1, -0.67, 0.0, -0.55, -0.12);
+        else if (mode == 6)
+            balance(R, G, B, 0.86, 0.0, 0.13, 0.57, 0.19, -0.52, 0.31, -0.30, -0.94);
+
         return;
     }
 
@@ -232,9 +341,14 @@ int main(int argc, char *argv[])
 
         printf("Defaults parameters are [-10,10] + [-10,10]i and xn = yn = 512.\n\n");
 
-        printf("A color function can be selected with -color. Valid options\n");
-        printf("are 0 (phase=hue, magnitude=brightness) and 1 (phase only,\n");
-        printf("white-gold-black-blue-white counterclockwise).\n\n");
+        printf("A color function can be selected with -color. The choices are:\n");
+        printf("0   phase=hue, magnitude=brightness\n");
+        printf("1   phase only, white-gold-black-blue-white counterclockwise\n");
+        printf("2   0+1 (dodge filter)\n");
+        printf("3   0+1, shiny\n");
+        printf("4   0+1, warm\n");
+        printf("5   0+1, cold\n");
+        printf("6   0+1, tomato\n\n");
 
         printf("The output is written to arbplot.ppm. If you have ImageMagick,\n");
         printf("run [convert arbplot.ppm arbplot.png] to get a PNG.\n\n");
