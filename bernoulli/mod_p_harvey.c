099ea357cdf40769258d15ef5746ccef3e347df3
fredrik <fredrik.johansson@gmail.com>
2021-10-28 23:30:18 +0200

    debug

--- bernoulli/mod_p_harvey.c
+++ bernoulli/mod_p_harvey.c
@@ -60,8 +60,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "flint/ulong_extras.h"
 #include "bernoulli.h"
 
-#define DEBUG 0
-#define TIMING 1
+#define DEBUG 1
+#define TIMING 0
 
 /******************************************************************************
 
