c3a944c54da9be88a86babf73e1948936b29655f
fredrik <fredrik.johansson@gmail.com>
2022-04-02 13:47:39 +0200

    fix a compiler warning

--- arf/mul_via_mpfr.c
+++ arf/mul_via_mpfr.c
@@ -12,7 +12,7 @@
 #include "arf.h"
 
 int
-arf_mul_via_mpfr(arf_ptr z, arf_srcptr x, arf_srcptr y,
+arf_mul_via_mpfr(arf_t z, const arf_t x, const arf_t y,
         slong prec, arf_rnd_t rnd)
 {
     mp_size_t xn, yn, zn, val;
